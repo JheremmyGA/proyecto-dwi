58513c2358cba5ffaa824bff32c6ea2c3429f445
JheremmyGA <56833777+JheremmyGA@users.noreply.github.com>
2025-08-17 22:23:41 -0500

    prueba clase producto

---
+++ BackEnd/icommerce/src/main/java/com/dwi/icommerce/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.dwi.icommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dwi.icommerce.model.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+
+}
