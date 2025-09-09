6736f913ef69f3884c7bcfb50dfeb8a1153bee33
JheremmyGA <56833777+JheremmyGA@users.noreply.github.com>
2025-09-08 20:17:23 -0500

    mejora de clasificacion de clases

--- BackEnd/icommerce/src/main/java/com/dwi/icommerce/controller/ProductoController.java
+++ BackEnd/icommerce/src/main/java/com/dwi/icommerce/controller/ProductoController.java
@@ -2,6 +2,7 @@ package com.dwi.icommerce.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import com.dwi.icommerce.service.ProductoService;
 @RequestMapping("/api/productos")
 @CrossOrigin(origins = "*")
 public class ProductoController {
+    
+    @Autowired
     private final ProductoService serviceProduct;
     
     public ProductoController(ProductoService serviceProduct){
--- BackEnd/icommerce/src/main/java/com/dwi/icommerce/repository/ProductoRepository.java
+++ BackEnd/icommerce/src/main/java/com/dwi/icommerce/repository/ProductoRepository.java
@@ -1,9 +1,11 @@
 package com.dwi.icommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dwi.icommerce.model.Producto;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
 
 }
