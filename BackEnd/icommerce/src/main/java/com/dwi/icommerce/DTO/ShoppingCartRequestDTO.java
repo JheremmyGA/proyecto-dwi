139918007d8681fe450cdfe35ea9e847a2822d22
JheremmyGA <56833777+JheremmyGA@users.noreply.github.com>
2025-11-14 20:46:29 -0500

    .

---
+++ BackEnd/icommerce/src/main/java/com/dwi/icommerce/DTO/ShoppingCartRequestDTO.java
@@ -0,0 +1,7 @@
+package com.dwi.icommerce.DTO;
+
+public class ShoppingCartRequestDTO {
+    public Long idUsuario;
+    public Long iDMainProduct;
+    public int cantidad;
+}
--- BackEnd/icommerce/src/main/java/com/dwi/icommerce/controller/InventarioController.java
+++ BackEnd/icommerce/src/main/java/com/dwi/icommerce/controller/InventarioController.java
@@ -1,14 +1,9 @@
 package com.dwi.icommerce.controller;
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.swing.text.StyledEditorKit.BoldAction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dwi.icommerce.DTO.InventarioGroupRequestDTO;
 import com.dwi.icommerce.DTO.InventarioItemDetailResponseDTO;
 import com.dwi.icommerce.DTO.InventarioItemUpdateStockDTO;
-import com.dwi.icommerce.model.Marca;
 import com.dwi.icommerce.service.InventarioService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
